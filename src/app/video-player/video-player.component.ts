f403079f8ddc0b21b82a64f735c9f86e18b49162
sandeep <sandeepsirodia@gmail.com>
2018-02-06 11:00:26 +0530

    mostly setup of videogular is done

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -26,16 +26,61 @@ export class VideoPlayerComponent implements OnInit {
 		this.globalListenFunc();
 	}
 
+	onClickPlayPause(event){
+		console.log(this.api)
+		if (this.api.state == "playing"){
+			this.api.pause();
+		}
+		else{
+			this.api.play();
+		}
+		
+	}
+
 	onPlayerReady(api:VgAPI) {
 	    this.api = api;
-	    this.globalListenFunc = this.renderer.listen('document', 'keypress', e => {
-			if (e.code == 'Space' && api.getDefaultMedia().state == "paused") {
+	    
+
+		this.globalListenFunc = this.renderer.listen('document', 'keydown', e => {
+			
+			if (e.key == ' ' && e.code == 'Space' && api.getDefaultMedia().state == "paused") {
 				this.api.play();
-			}else{
+			}else if (e.key == ' ' && e.code == 'Space' && api.getDefaultMedia().state != "paused"){
 				this.api.pause()
 			}
-			if (e.code == 'KeyF' ) {
+
+			if (e.key == 'f' &&  e.key != ' ' ) {
 				this.api.fsAPI.toggleFullscreen();
+			}
+			
+			
+				
+			if (e.code == 'ArrowLeft') {
+				this.api.currentTime -= 5;
+			}
+
+			if (e.code == 'ArrowRight') {
+				this.api.currentTime += 10;
+			}
+
+			// else{
+			// 	this.api.pause()
+			// }
+			if (e.code == 'ArrowUp'){
+				if (this.api.volume < .95) {
+					this.api.volume = this.api.volume + .05;	
+				}else{
+					this.api.volume = 1;
+				}
+				
+			}
+			if (e.code == 'ArrowDown') {
+				if (this.api.volume > .05) {
+					this.api.volume = this.api.volume - .05;	
+				}else{
+					this.api.volume = 0;
+				}
+				
 			}
 		});
 	    
