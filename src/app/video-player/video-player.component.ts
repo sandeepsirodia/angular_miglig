ba881689387841cca9b7ce487a93d12b41680939
sandeep <sandeepsirodia@gmail.com>
2018-02-06 09:38:37 +0530

    space bar and f button

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, ViewChild,ElementRef,AfterContentInit, Renderer2} from '@angular/core';
+import {VgAPI} from 'videogular2/core';
 
 @Component({
   selector: 'app-video-player',
@@ -7,20 +8,44 @@ import {Component, OnInit, ViewChild,ElementRef,AfterContentInit, Renderer2} fro
   
 })
 export class VideoPlayerComponent implements OnInit {
-
+	preload:string = 'auto';
+    api:VgAPI;
 	globalListenFunc: Function;
 
 	constructor(private renderer: Renderer2) {}
 
 	ngOnInit() {
 		this.globalListenFunc = this.renderer.listen('document', 'keypress', e => {
-			console.log(e);
+			// console.log(e.code);
+			
 		});
 	}
 
 	ngOnDestroy() {
 		// remove listener
 		this.globalListenFunc();
+	}
+
+	onPlayerReady(api:VgAPI) {
+	    this.api = api;
+	    this.globalListenFunc = this.renderer.listen('document', 'keypress', e => {
+			if (e.code == 'Space' && api.getDefaultMedia().state == "paused") {
+				this.api.play();
+			}else{
+				this.api.pause()
+			}
+			if (e.code == 'KeyF' ) {
+				this.api.fsAPI.toggleFullscreen();
+			}
+		});
+	    
+	    // console.log(this.api.getDefaultMedia())
+	    this.api.getDefaultMedia().subscriptions.ended.subscribe(
+	        () => {
+	            this.api.getDefaultMedia().currentTime = 0;
+	            
+	        }
+	    );
 	}
 
 }
